51f7884f2744187e3de00809380db91675961c44
woodrex83 <woodrex83@gmail.com>
2024-03-10 05:21:35 +0000

    test(kafka): consumer test

---
+++ kafka_consumer/test.py
@@ -0,0 +1,21 @@
+import asyncio
+from typing import Optional
+
+from kafka_consumer.consumer import KafkaConsumer
+from kafka_consumer.setting import settings
+
+async def message_log(message: str, headers: Optional[dict]):
+    # Efficiency drop if using loguru instead of print
+    # logger.debug(f"Received message: {message}, headers: {headers}")
+    print(f"Received message: {message}, headers: {headers}")
+
+async def main():
+    try:
+        c = KafkaConsumer(settings.kafka)
+        async for message, headers in c:
+            await message_log(message, headers)
+    except KeyboardInterrupt:
+        await c.close()
+        
+if __name__ == '__main__':
+    asyncio.run(main())
